936be09405cdcf4b5c0cbc02ba9ad4498a6df866
AD3600 <45272213+AD3600@users.noreply.github.com>
2019-03-07 20:56:50 -0500

    Update ride.cpp

--- USACO Training Gateway/1.2/ride.cpp
+++ USACO Training Gateway/1.2/ride.cpp
@@ -20,7 +20,7 @@ int main()
     freopen("ride.out", "w", stdout);
     string s1, s2; cin >> s1 >> s2;
     ll prod1 = 1, prod2 = 1;
-    for(unsigned int i = 0; i < s1.length(); i++) prod1 = (prod1 * (((int) (s1[i] - 'A' + 1)) % 47)) % 47;
+    for(unsigned int i = 0; i < s1.length(); i++) prod1 = (prod1 * (((int) (s1[i] - 'A' + 1)) % 47) % 47);
     for(unsigned int i = 0; i < s2.length(); i++) prod2 = (prod2 * (((int) (s2[i] - 'A' + 1)) % 47) % 47);
     if(prod1 == prod2) cout << "GO" << "\n";
     else cout << "STAY" << "\n";
