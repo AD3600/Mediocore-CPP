8a1eddb02a5d81844c3f235d04e99f742ae56f3c
AD3600 <45272213+AD3600@users.noreply.github.com>
2019-03-07 20:41:45 -0500

    Create template.cpp

---
+++ template.cpp
@@ -0,0 +1,22 @@
+/*
+ID: ad36051
+LANG: C++
+TASK: 
+*/
+
+#include <iostream>
+#include <fstream>
+#include <algorithm>
+#include <string>
+#include <cstring>
+#include <math.h>
+
+typedef long long ll;
+using namespace std;
+
+int main()
+{
+    freopen("text.in", "r", stdin);
+    freopen("text.out", "w", stdout);
+    
+}
