9307b502dc8785b6709b4acc7bd8ad7b71a49a67
AD3600 <45272213+AD3600@users.noreply.github.com>
2019-06-20 19:40:32 -0400

    Update ctiming.cpp

--- USACO Contests/2011 - 2012/November/Bronze/ctiming.cpp
+++ USACO Contests/2011 - 2012/November/Bronze/ctiming.cpp
@@ -1,11 +1,14 @@
 // Problem 1. Contest Timing
 #include <iostream>
+#include <fstream>
 
 using namespace std;
 
 int D, H, M;
 
 int main() {
+    // freopen("ctiming.in", "r", stdin);
+    // freopen("ctiming.out", "w", stdout);
     cin >> D >> H >> M;
     int ans = 24 * 60 * (D - 11) + 60 * (H - 11) + (M - 11);
     if(ans < 0)
